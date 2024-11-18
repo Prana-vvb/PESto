2b81e9fb07dccb8e1be54f4abf825506689bed86
Prana-vvb <pvb1532005@gmail.com>
2024-11-18 22:24:51 +0530

    refactor note-keeper

--- note-keeper/src/App.js
+++ note-keeper/src/App.js
@@ -1,33 +1,32 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import Note from "./Note";
 
 function App() {
-  const [notes, setNotes] = useState([{ id: Date.now() }]);
+    const [notes, setNotes] = useState([{ id: Date.now() }]);
 
-  const addNote = () => {
-    setNotes((prevNotes) => [...prevNotes, { id: Date.now() }]);
-  };
+    const addNote = () => {
+        setNotes((prevNotes) => [...prevNotes, { id: Date.now() }]);
+    };
 
-  const deleteNote = (id) => {
-    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
-  };
+    const deleteNote = (id) => {
+        setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+    };
 
-  return (
-    <div>
-      <Header />
-      <button className="add-note-button" onClick={addNote}>
-        Add Note
-      </button>
-      <div className="notes-container">
-        {notes.map((note) => (
-          <Note key={note.id} id={note.id} onDelete={deleteNote} />
-        ))}
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <h1><span className="pes" >PES</span><span className="to">to</span></h1>
+            </header>
+            <button className="add-note-button" onClick={addNote}>
+                Add Note
+            </button>
+            <div className="notes-container">
+                {notes.map((note) => (
+                    <Note key={note.id} id={note.id} onDelete={deleteNote} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
